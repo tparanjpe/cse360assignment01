eed7d8008d3b11a2371a8bcdaf544d597c2addfe
Tara Paranjpe <tara.paranjpe@live.com>
2020-10-06 18:09:20 -0700

    first commit

---
+++ src/cse360assignment01/AddingMachine.java
@@ -0,0 +1,49 @@
+package cse360assignment01;
+
+public class AddingMachine {
+    private int total;
+
+    /**
+     * Creates an Adding Machine object
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * Gets the first and last name of this Student.
+     * @return this Student's name.
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /**
+     * Adds the value passed in
+     * @param integer value for desired addition
+     */
+    public void add (int value) {
+
+    }
+
+    /**
+     * Subtract the value passed in
+     * @param integer value for desired subtraction
+     */
+    public void subtract (int value) {
+
+    }
+    /**
+     *converts string output of referenced value to a string
+     * @returns string output of referenced value
+     */
+    public String toString () {
+        return "";
+    }
+    /**
+     * Clears
+     */
+    public void clear() {
+
+    }
+}
